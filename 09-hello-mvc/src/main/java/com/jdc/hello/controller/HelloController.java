e63905a4e966e7b9360affa72a5f108ca7a9ea22
Htun Khaing Linn <htunkhainglynn13@gmail.com>
2023-02-14 23:47:46 +0630

    add configuration via deployment descriptor

---
+++ 09-hello-mvc/src/main/java/com/jdc/hello/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.jdc.hello.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jdc.hello.model.HelloService;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@Autowired
+	HelloService service;
+	
+	@GetMapping
+	public String index(Model model) {
+		model.addAttribute("message", service.getMessage());
+		return "index";
+	}
+}
