d98385557ce60690b8813e6be1adf47594bb8d22
Htun Khaing Linn <htunkhainglynn13@gmail.com>
2023-02-20 17:26:54 +0630

    change back to xml to solve a problem : README

--- 10-request-mapping/src/main/java/com/jdc/mapping/controller/CourseController.java
+++ 10-request-mapping/src/main/java/com/jdc/mapping/controller/CourseController.java
@@ -2,7 +2,7 @@ package com.jdc.mapping.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,15 +16,15 @@ public class CourseController {
 	@Autowired
 	private CourseService service;
 
-//	@GetMapping  // do not work in xml style
-//	public String index(Model model) {
-//		model.addAttribute("list", service.getAll());
-//		return "course";
-//	}
-	
-	@GetMapping
-	public String index(ModelMap model) {
-		model.put("list", service.getAll());
+	@GetMapping  
+	public String index(Model model) {
+		model.addAttribute("list", service.getAll());
 		return "course";
 	}
+	
+//	@GetMapping
+//	public String index(ModelMap model) {
+//		model.put("list", service.getAll());
+//		return "course";
+//	}
 }
